947f569a725baed4affaf752c8042eaf46e65e06
StijnDB6 <r0942487@student.thomasmore.be>
2024-11-25 15:46:53 +0100

    exclude datasource review

--- review-service/src/main/java/fact/it/reviewservice/ReviewServiceApplication.java
+++ review-service/src/main/java/fact/it/reviewservice/ReviewServiceApplication.java
@@ -2,8 +2,9 @@ package fact.it.reviewservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class ReviewServiceApplication {
 
     public static void main(String[] args) {
