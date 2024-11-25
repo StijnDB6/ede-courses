2c81fa5560e803dc4a7fa8f29888bf12b28ddb4a
StijnDB6 <r0942487@student.thomasmore.be>
2024-11-25 15:37:45 +0100

    ECode change to eCode

--- course-service/src/main/java/fact/it/courseservice/repository/CourseRepository.java
+++ course-service/src/main/java/fact/it/courseservice/repository/CourseRepository.java
@@ -3,6 +3,8 @@ package fact.it.courseservice.repository;
 
 import fact.it.courseservice.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,6 +13,7 @@ import java.util.List;
 @Repository
 @Transactional
 public interface CourseRepository extends JpaRepository<Course, Long> {
-    List<Course> findByECodeIn(List<String> eCode);
+    @Query("Select c FROM Course c WHERE c.eCode IN :eCodes")
+    List<Course> findByECodeIn(@Param("eCodes") List<String> eCode);
     List<Course> findCourseByOpenSpotsGreaterThan(int greater);
 }
