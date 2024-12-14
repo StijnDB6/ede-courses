1cb986c6789a5dc9668bf6ff81f3015f8992aef7
StijnDB6 <r0942487@student.thomasmore.be>
2024-12-14 19:30:21 +0100

    test redis

--- api-gateway/src/main/java/fact/it/apigateway/config/RateLimiterConfig.java
+++ api-gateway/src/main/java/fact/it/apigateway/config/RateLimiterConfig.java
@@ -1,6 +1,7 @@
 package fact.it.apigateway.config;
 
 import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
@@ -9,7 +10,17 @@ import reactor.core.publisher.Mono;
 public class RateLimiterConfig {
 
     @Bean
-    public KeyResolver remoteAddressKeyResolver() {
+    public KeyResolver userKeyResolver() {
+        return exchange -> Mono.just("1"); // Use a unique key for each user
+    }
+
+    @Bean
+    public RedisRateLimiter redisRateLimiter() {
+        return new RedisRateLimiter(10, 20); // 10 requests per second with a burst capacity of 20
+    }
+
+    @Bean
+    public KeyResolver remoteAddrKeyResolver() {
         return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
     }
 }
